e6617b5c2118d2234f076e7a415b4c2e3cfd8767
Fabien Mathieu <fabien.mathieu@normalesup.org>
2023-09-13 17:42:29 +0200

    First commit

---
+++ package_helper_3/foo.py
@@ -0,0 +1,46 @@
+def foo() -> str:
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        foo (str): Description of arg1
+
+    Returns:
+        str: Description of return value
+
+    Examples
+    --------
+
+    >>> foo()
+    'foo'
+    """
+
+    return "foo"
+
+
+def fibo(n):
+    """
+
+    Parameters
+    ----------
+    n: :class:`int`
+        The sequence number
+
+    Returns
+    -------
+    :class:`int`
+
+    Examples
+    --------
+
+    >>> fibo(3)
+    2
+    >>> fibo(30)
+    832040
+    """
+    return 1 if n < 3 else fibo(n-1)+fibo(n-2)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    pass
---
+++ tests/test_foo.py
@@ -0,0 +1,5 @@
+from package_helper_3.foo import foo
+
+
+def test_foo():
+    assert foo() == "foo"
