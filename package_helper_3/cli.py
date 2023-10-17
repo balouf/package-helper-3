214a2796b2594861f272ee073789f1ca5adf9bb6
fabien <fabien.mathieu@nokia-bell-labs.com>
2023-10-17 17:20:02 +0200

    Pragma cli

--- package_helper_3/cli.py
+++ package_helper_3/cli.py
@@ -1,7 +1,7 @@
 import os
 
 
-def main() -> None:
+def main() -> None: # pragma: no cover
     cwd = os.path.dirname(__file__)
     package_dir = os.path.abspath(os.path.join(cwd, ".."))
     os.system(f"cookiecutter {package_dir}")
