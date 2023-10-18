2613848625e9391f1eff12c29ffaf9c18fa89a38
fabien <fabien.mathieu@nokia-bell-labs.com>
2023-10-18 13:33:32 +0200

    wip

--- package_helper_3/cli.py
+++ package_helper_3/cli.py
@@ -1,7 +1,15 @@
 import os
+import click
 
 
+@click.command()
+@click.version_option(message="Package Helper 3, version %(version)s")
 def main() -> None: # pragma: no cover
+    """
+    Console script for Package Helper 3.
+
+    Cf https://balouf.github.io/package-helper-3/
+    """
     cwd = os.path.dirname(__file__)
     package_dir = os.path.abspath(os.path.join(cwd, ".."))
     os.system(f"cookiecutter {package_dir}")
